00b4c5a84a5539872c71575a8660180fed194c2d
Piyush Sharma <autri.piyush@gmail.com>
2025-08-20 23:32:41 +0530

    Delete Program_2 directory

--- Program_2/1(a).py
+++
@@ -1,9 +0,0 @@
-def bubble_sort(a):
-    n=len(a)
-    for i in range(n-1):
-        for j in range(n-i-1):
-            if a[j]>a[j+1]:
-                a[j],a[j+1]=a[j+1],a[j]
-a=[5,2,9,1,5,6]
-bubble_sort(a)
-print(a)
